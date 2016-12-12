d197d6fc35fdd536185437d5f65d46cf8a8884b5
judge2020 <hunterray55@hotmail.com>
2016-12-12 09:58:17 -0500

    main part is done, need help finding best color ranges.

---
+++ LootWatcher/BoxWatcher.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+using Timer = System.Timers.Timer;
+
+namespace LootWatcher
+{
+	class BoxWatcher
+	{
+		public bool showNotifications { get; set; }
+		public bool legendarySound { get; set; }
+
+
+	}
+}
---
+++ LootWatcher/UI/TrayIcon.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows;
+using System.Drawing;
+
+namespace LootWatcher
+{
+	class TrayIcon
+	{
+		public ContextMenu trayMenu { get; }
+		public NotifyIcon trayIcon { get; }
+		public TrayIcon()
+		{
+			trayIcon = new NotifyIcon();
+			trayIcon.Text = "LootWatcher";
+			trayIcon.Icon = new Icon(SystemIcons.Application, 40, 40);
+			trayIcon.ContextMenu = trayMenu;
+			trayIcon.Visible = true;
+			trayIcon.Click += TrayIconOnClick;
+			
+		}
+
+		private void TrayIconOnClick(object sender, EventArgs eventArgs)
+		{
+			Environment.Exit(0);
+		}
+
+		public void ShowNotification(string title, string text)
+		{
+			trayIcon.ShowBalloonTip(5, title, title, ToolTipIcon.Info);
+		}
+	}
+}
